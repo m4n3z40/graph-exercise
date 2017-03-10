b24d559ea20b758b880d89f9eef1b42d0cc28ef5
Allan Baptista <abaptista@daitangroup.com>
2017-03-10 15:12:59 -0300

    Finished fast implementation of algorithm

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const RomaniaFactory = require('./lib/countries/Romania');
+const BFSAlgorithm = require('./lib/algorithms/BFSAlgorithm');
 const DFSAlgorithm = require('./lib/algorithms/DFSAlgorithm');
 
-const Romania = RomaniaFactory.create(new DFSAlgorithm());
+function resultsForAlgorithm(algorithm) {
+    const Romania = RomaniaFactory.create(algorithm);
 
-const {path, totalDistance} = Romania.calculatePathAndDistance('Arad', 'Bucharest');
+    return Romania.calculatePathAndDistance('Arad', 'Bucharest');
+}
 
-console.log(path.map(city => city.name).join(' -> '));
-console.log(`Total Distance$ ${totalDistance}`);
+function logResultsFor(title, {path, totalDistance}) {
+    console.log(
+`
+===============================================
+
+${title}
+Path: ${path.map(city => city.name).join(' -> ')};
+Total Distance: ${totalDistance}
+
+===============================================`
+    );
+}
+
+function logBestResult(bestResultName, bestResult, worstResult) {
+    console.log(
+`
+${bestResultName} was the best result! 
+Total distance: ${bestResult.totalDistance}
+Diff: ${bestResult.totalDistance - worstResult.totalDistance}`
+    );
+}
+
+const BFSResults = resultsForAlgorithm(new BFSAlgorithm);
+const DFSResults = resultsForAlgorithm(new DFSAlgorithm);
+
+logResultsFor('BFS Algorithm', BFSResults);
+logResultsFor('DFS Algorithm', DFSResults);
+
+if (BFSResults.totalDistance < DFSResults.totalDistance) {
+    logBestResult('BFS', BFSResults, DFSResults);
+} else {
+    logBestResult('DFS', BFSResults, DFSResults);
+}
